91034ec843aa018527ccae7522d3b52f475085c5
유영신 <yooyoosoo@naver.com>
2024-07-23 13:21:13 +0900

    과연

---
+++ SSAFY_8/_2024_07_29/이주호/입문/대문자와소문자.java
@@ -0,0 +1,21 @@
+package _2024_07_29.이주호.입문;
+
+public class 대문자와소문자 {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i =0;i<my_string.length();i++){
+            char c = my_string.charAt(i);
+            if (Character.isUpperCase(c)){
+                answer+=Character.toLowerCase(c);
+            }else{
+                answer+=Character.toUpperCase(c);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        대문자와소문자 s = new 대문자와소문자();
+        System.out.println(s.solution("cccCCC"));
+    }
+}
