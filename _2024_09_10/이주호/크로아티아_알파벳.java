4d6d7c112e5c579729b8b184b48438db2bd47387
이주호 <139448668+lsc713@users.noreply.github.com>
2024-09-10 23:11:46 +0900

    Update 크로아티아_알파벳.java

--- _2024_09_10/이주호/크로아티아_알파벳.java
+++ _2024_09_10/이주호/크로아티아_알파벳.java
@@ -5,6 +5,7 @@ class 크로아티아_알파벳 {
     public static void main(String[] args) throws NumberFormatException, IOException {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        //문제에서 해당 내용은 한 글자임을 제시. 이에 대해 임의의 문자로 변환 및 문제에서 요구하는 문자의 길이 반환
         input = input.replaceAll("c=", "1");
         input = input.replaceAll("c-", "2");
         input = input.replaceAll("dz=", "3");
