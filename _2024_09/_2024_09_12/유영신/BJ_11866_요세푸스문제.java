f91395775ece0e6fd6d1286d496158031aadcf03
유영신 <yooyoosoo8989@gmail.com>
2024-09-12 17:19:02 +0900

    📝백준11866 문제풀이

--- _2024_09/_2024_09_12/유영신/BJ_11866_요세푸스문제.java
+++ _2024_09/_2024_09_12/유영신/BJ_11866_요세푸스문제.java
@@ -3,6 +3,7 @@ package _2024_09._2024_09_12.유영신;
 import java.io.*;
 import java.util.*;
 
+// 리스트를 이용할 수 있지만, 인덱스 설정이 어려울 것 같아서 큐로 구현
 public class BJ_11866_요세푸스문제 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,13 +20,13 @@ public class BJ_11866_요세푸스문제 {
 			queue.add(i);
 		}
 //       System.out.println(queue);
-		while (!queue.isEmpty()) {
-			for (int i = 0; i < K - 1; i++) {
+		while (!queue.isEmpty()) { // 큐가 빌때까지 반복
+			for (int i = 0; i < K - 1; i++) { // K = 3이라면 2명만 poll해주고 다시 add
 				int tmp = queue.poll();
 				queue.add(tmp);
 			}
-			int result = queue.poll();
-			sb.append(result).append(",").append(" ");
+			int result = queue.poll(); // 세번째 사람은 poll 해주고
+			sb.append(result).append(",").append(" "); // 결과값에 추가
 
 		}
 		sb.delete(sb.length() - 2, sb.length());
