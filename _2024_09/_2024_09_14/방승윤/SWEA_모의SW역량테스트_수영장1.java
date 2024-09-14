bd56ae951d290b61dbb01808eef943d5c1aa2d7b
방승윤 <sybang98@gmail.com>
2024-09-15 00:48:08 +0900

    📝 SWEA 모의SW역량테스트 수영장 문제풀이

---
+++ _2024_09/_2024_09_14/방승윤/SWEA_모의SW역량테스트_수영장1.java
@@ -0,0 +1,69 @@
+package _2024_09._2024_09_14.방승윤;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA_모의SW역량테스트_수영장1 {
+	
+	static int[] price = new int[4];
+	static int[][] month = new int[12][2];
+	static int cost;
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(st.nextToken());
+		
+		for (int tc = 1; tc <= T; tc++) {
+			
+			st = new StringTokenizer(br.readLine());
+			
+			for (int i = 0; i < 4; i++) {
+				price[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			st = new StringTokenizer(br.readLine());
+			
+			for (int i = 0; i < 12; i++) {
+				month[i][0] = Integer.parseInt(st.nextToken());
+				month[i][1] = Math.min(price[0] * month[i][0], price[1]);
+				cost += month[i][1];
+			}
+			
+			findMin(0, 0);
+			
+			sb.append("#").append(tc).append(" ").append(cost).append("\n");
+			
+			cost = 0;
+			
+		}
+		
+		System.out.println(sb);
+		
+	}
+	
+	static void findMin(int idx, int sum) {
+		
+		if (sum > cost) {
+			return;
+		}
+		
+		if (idx >= 12) {
+			cost = Math.min(cost, sum);
+			return;
+		}
+		
+		if (month[idx][0] == 0) {
+			findMin(idx + 1, sum);			
+		} else {
+			findMin(idx + 1, sum + month[idx][1]);
+			findMin(idx + 3, sum + price[2]);
+			findMin(idx + 12, sum + price[3]);
+		} 
+		
+	}
+	
+}
