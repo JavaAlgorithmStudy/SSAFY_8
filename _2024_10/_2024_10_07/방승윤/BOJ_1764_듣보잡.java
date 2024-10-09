1124077741f386d14794cd295d86b2f9373addc0
방승윤 <sybang98@gmail.com>
2024-10-09 19:47:41 +0900

    📝 백준1764 수정

--- _2024_10/_2024_10_07/방승윤/BOJ_1764_듣보잡.java
+++ _2024_10/_2024_10_07/방승윤/BOJ_1764_듣보잡.java
@@ -2,10 +2,6 @@ package _2024_10._2024_10_07.방승윤;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class BOJ_1764_듣보잡 {
@@ -14,7 +10,6 @@ public class BOJ_1764_듣보잡 {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
 
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
