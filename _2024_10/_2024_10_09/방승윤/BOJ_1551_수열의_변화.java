16123c2c0cc84068a8cd50a8304fa3c27e344512
방승윤 <sybang98@gmail.com>
2024-10-09 21:23:01 +0900

    📝 백준 문제풀이

---
+++ _2024_10/_2024_10_09/방승윤/BOJ_1551_수열의_변화.java
@@ -0,0 +1,46 @@
+package _2024_10._2024_10_09.방승윤;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1551_수열의_변화 {
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine(), ",");
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < K; i++) {
+			N--;
+			
+			for (int j = 0; j < N; j++) {
+				arr[j] = arr[j + 1] - arr[j];
+			}
+		}
+			
+		
+		for (int i = 0; i < N; i++) {
+			sb.append(arr[i]);
+			
+			if (i != N - 1) {
+				sb.append(",");
+			}
+		}
+		
+		System.out.println(sb);
+		
+	}
+	
+}
