c06bb7abf6cbe8dfa26c2ffee761aecbf020937c
방승윤 <sybang98@gmail.com>
2024-10-15 00:49:47 +0900

    📝 백준1541 수정

--- _2024_10/_2024_10_14/방승윤/BOJ_1541_잃어버린_괄호.java
+++ _2024_10/_2024_10_14/방승윤/BOJ_1541_잃어버린_괄호.java
@@ -3,8 +3,6 @@ package _2024_10._2024_10_14.방승윤;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_1541_잃어버린_괄호 {
@@ -13,7 +11,6 @@ public class BOJ_1541_잃어버린_괄호 {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), "-");
-		Queue<Integer> queue = new LinkedList<>();
 		StringTokenizer stNum = new StringTokenizer(st.nextToken(), "+");
 		int ans = 0;
 		
