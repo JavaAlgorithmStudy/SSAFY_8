0af0dc402fc9da49b40b2dbc7517fd0da0976a24
unknown <sybang98@gmail.com>
2024-10-25 23:01:43 +0900

    📝 백준21736 문제풀이

---
+++ _2024_10/_2024_10_25/방승윤/BOJ_21736_헌내기는_친구가_필요해.java
@@ -0,0 +1,65 @@
+package _2024_10._2024_10_25.방승윤;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_21736_헌내기는_친구가_필요해 {
+	
+	static int N, M, friend;
+	static char[][] campus;
+	static boolean[][] visited;
+	static int[] dx = {0, 1, 0, -1};
+	static int[] dy = {-1, 0, 1, 0};
+	
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		campus = new char[N][M];
+		visited = new boolean[N][M];
+		
+		
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+			
+			for (int j = 0; j < M; j++) {
+				campus[i][j] = line.charAt(j);
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (campus[i][j] == 'I') {
+					find(i, j);
+					break;
+				}
+			}
+		}
+		
+		System.out.println(friend == 0 ? "TT" : friend);
+
+	}
+
+	private static void find(int x, int y) {
+		visited[x][y] = true;
+		
+		if (campus[x][y] == 'P') {
+			friend++;
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny] && campus[nx][ny] != 'X') {
+				find(nx, ny);
+			}
+		}
+		
+	}
+
+}
